238837d2e510e9ac92c0a6e4d45cb1ca1f9452c2
marcusrelacion <hello@marcusrelacion.com>
2018-09-11 19:22:09 -0400

    removing app.js script, removing old OG images, updating HEAD

---
+++ assets/js/app.js
@@ -0,0 +1,6 @@
+//Offside.js minimal set
+var myOffside = offside('#mobile-menu', {
+  slidingElementsSelector: '#site-container',
+  buttonsSelector: '#mobile-menu-trigger',
+  slidingSide: 'right' // String: Offside element pushed on left or right
+});
