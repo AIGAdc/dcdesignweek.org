c73da5b98d540235d76784ae516f6bed85e1a8eb
Marcus Relacion <mr.general@fastmail.com>
2019-04-26 19:10:37 -0400

    removing unused js libraries

--- assets/js/app.js
+++
@@ -1,6 +0,0 @@
-//Offside.js minimal set
-var myOffside = offside('#mobile-menu', {
-  slidingElementsSelector: '#site-container',
-  buttonsSelector: '#mobile-menu-trigger',
-  slidingSide: 'right' // String: Offside element pushed on left or right
-});
